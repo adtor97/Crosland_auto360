778d0767d122796bb30e84486574d00790d0d082
unknown <adrian135torrejon@gmail.com>
2020-10-13 02:02:55 -0500

    final commit monday. improve templates

--- app.py
+++ app.py
@@ -9,24 +9,30 @@ Created on Thu Oct  8 20:43:57 2020
 from flask import Flask, render_template, request, redirect, url_for, session, Markup
 import pandas as pd
 from datetime import date
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import os
 #from flask_login import LoginManager
 #from requests import request
 
 from utils import utils_google, utils_data_wrangling, utils_plotly
 
 app = Flask(__name__)
-today = str(date.today())
-app.secret_key = today
-
-df_results = utils_google.read_ws_data(utils_google.open_ws("Crosland_data_master", "base_general"))
-df_results["value"] = df_results["value"].astype(float)
-
 #login_manager = LoginManager()
 #login_manager.init_app(app)
+#today = str(date.today())
+#app.secret_key = today
+
+df_results = utils_google.read_ws_data(utils_google.open_ws("Crosland_data_master", "base_general"))
+df_results
+df_results["value"] = df_results["value"].astype(float)
+#df_results["Q"] = utils_data_wrangling.get_quarter
+static_folder = os.path.join('static')
 
 @app.route("/")
 def home():
-    return render_template("home_html.html")
+    grafo = os.path.join(static_folder, 'Grafo.png')
+    return render_template("home_html.html", grafo = grafo)
 
 @app.route("/login_admin")
 def login_admin():
@@ -40,14 +46,6 @@ def login_coll():
 def coll_results_redirect():
     if request.method == 'POST':
         DNI = request.form["DNI"]
-        #birthday_year = request.form["birthday_year"]
-        df_results["DNI_evaluado"]  = df_results["DNI_evaluado"].astype(str)
-        df_results_DNI = df_results.loc[df_results["DNI_evaluado"] == str(DNI)]
-        #print(df_results_DNI)
-        #df_results_DNI["Fecha Nacimiento_evaluado"] = pd.to_datetime(df_results_DNI["Fecha Nacimiento_evaluado"])
-        #df_results_DNI = df_results.loc[df_results["Fecha Nacimiento_evaluado"] == birthday_year]
-
-        session["df_results_DNI"] = df_results_DNI[["value", "Pilar"]].to_json()
 
         return redirect(url_for("coll_results", DNI=DNI))
 
@@ -57,16 +55,15 @@ def coll_results_redirect():
 
 @app.route("/coll_results/<DNI>", methods=["GET"])
 def coll_results(DNI):
-    df_results_DNI=pd.read_json(session["df_results_DNI"])
-    #print(type(df_results_DNI), type(df_results))
-    #print(df_results_DNI)
+    df_results["DNI_evaluado"]  = df_results["DNI_evaluado"].astype(str)
+    df_results_DNI = df_results.loc[df_results["DNI_evaluado"] == str(DNI)]
 
     if len(df_results_DNI) == 0:
         return render_template("fail_login_coll_html.html")
 
     else:
 
-        radar = utils_plotly.build_radar(df_results, df_results_DNI)
+        radar = utils_plotly.build_radar_coll(df_results[["Pilar", "value"]], df_results_DNI[["Pilar", "value"]])
 
         #return "hola"
         return render_template("coll_results_html.html", div_radar = radar)
@@ -129,18 +126,20 @@ def see_results():
 
             prom = round(df_complete.value.mean(), 2)
 
-            prom_contagiamos = round(df_complete.loc[df_complete.Pilar == "Contagiamos pasión"].value.mean(), 2)
-            prom_excelencia = round(df_complete.loc[df_complete.Pilar == "Buscamos la excelencia"].value.mean(), 2)
-            prom_juntos = round(df_complete.loc[df_complete.Pilar == "Trabajamos juntos"].value.mean(), 2)
-            prom_disfrutamos = round(df_complete.loc[df_complete.Pilar == "Vivimos y disfrutamos"].value.mean(), 2)
+            #prom_contagiamos = round(df_complete.loc[df_complete.Pilar == "Contagiamos pasión"].value.mean(), 2)
+            #prom_excelencia = round(df_complete.loc[df_complete.Pilar == "Buscamos la excelencia"].value.mean(), 2)
+            #prom_juntos = round(df_complete.loc[df_complete.Pilar == "Trabajamos juntos"].value.mean(), 2)
+            #prom_disfrutamos = round(df_complete.loc[df_complete.Pilar == "Vivimos y disfrutamos"].value.mean(), 2)
 
-            proms = [prom_contagiamos, prom_excelencia, prom_juntos, prom_disfrutamos]
-            values = df_complete.Pilar.unique()
+            radar = utils_plotly.build_radar_general(df_complete[["Pilar", "value"]])
+
+            #proms = [prom_contagiamos, prom_excelencia, prom_juntos, prom_disfrutamos]
+            #values = df_complete.Pilar.unique()
 
             return render_template('show_initial_results.html',
                                     tables=[df_complete_show.to_html(classes='data')],
                                     titles=df_complete_show.columns.values,
-                                    prom = prom, proms = proms)
+                                    prom = prom, div_radar = radar)
 
         except:
 
@@ -161,4 +160,4 @@ def final_page():
 
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
--- utils/utils_plotly.py
+++ utils/utils_plotly.py
@@ -1,7 +1,7 @@
 from plotly import graph_objects as go
 import pandas as pd
 
-def build_radar(df_total, df_coll):
+def build_radar_coll(df_total, df_coll):
 
     df_pilares_grouped = df_total.groupby("Pilar", as_index = False).mean()
     df_pilares_coll_grouped = df_coll.groupby("Pilar", as_index = False).mean()
@@ -22,3 +22,31 @@ def build_radar(df_total, df_coll):
 
     fig = fig.to_html(full_html=False)
     return fig
+
+def build_radar_general(df_total):
+
+    df_pilares_grouped = df_total.groupby("Pilar", as_index = False).mean()
+
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatterpolar(
+        r = df_pilares_grouped['value'].append(pd.Series(df_pilares_grouped['value'][0])),
+        theta = df_pilares_grouped['Pilar'].append(pd.Series(df_pilares_grouped['Pilar'][0])),
+        name = "Promedio general"
+    ))
+
+    fig = fig.to_html(full_html=False)
+    return fig
+
+def build_lines_coll(df_total, df_coll):
+
+    df_pilares_grouped = df_total.groupby("Pilar", as_index = False).mean()
+    df_pilares_coll_grouped = df_coll.groupby("Pilar", as_index = False).mean()
+
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatterpolar(
+        r = df_pilares_grouped['value'].append(pd.Series(df_pilares_grouped['value'][0])),
+        theta = df_pilares_grouped['Pilar'].append(pd.Series(df_pilares_grouped['Pilar'][0])),
+        name = "Promedio general"
+    ))
