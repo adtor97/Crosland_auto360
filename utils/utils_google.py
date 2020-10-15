5041625b798b8852663647911ef4aa51133c720e
unknown <adrian135torrejon@gmail.com>
2020-10-15 05:15:22 -0500

    check rq error

--- utils/utils_google.py
+++ utils/utils_google.py
@@ -23,7 +23,7 @@ def pandas_to_sheets(pandas_df, sheet, clear = True):
     sheet.update_cells(cells)
 
 def open_ws(sheet, ws_name):
-    scope = ['https://spreadsheets.google.com/feeds', 
+    scope = ['https://spreadsheets.google.com/feeds',
             'https://www.googleapis.com/auth/drive']
 
     credentials = ServiceAccountCredentials.from_json_keyfile_name(
@@ -42,4 +42,4 @@ def read_ws_data(ws):
 
     df = pd.DataFrame(data, columns=headers)
 
-    return df
+    return df.head()
