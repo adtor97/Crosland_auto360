9f4b7fb0246e555ca6144276fd7b92c0b356f0f9
unknown <adrian135torrejon@gmail.com>
2020-10-19 21:33:07 -0500

    pruebas redistogo

--- worker.py
+++ worker.py
@@ -5,7 +5,7 @@ from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
-redis_url = os.getenv('redis://redistogo:7acd2afb36ddae5c4d83946cc59fff56@barb.redistogo.com:10270/', 'redis://localhost:6379')
+redis_url = os.getenv('redis://redistogo:7acd2afb36ddae5c4d83946cc59fff56@barb.redistogo.com:10270/', 'redis://localhost:10270')
 
 conn = redis.from_url(redis_url)
 
